c3a4e3f58bda2b30fd95dc2243e8d2a510cea52b
thomas fischer <4tofi@gmx.net>
2020-10-29 21:23:22 +0100

    regex angepasst

--- tanken/tanken.sh
+++ tanken/tanken.sh
@@ -9,12 +9,12 @@ holePreise(){
     name=$1
     url=$2
     printf "$t $name "
-    curl $url | fgrep "init(" | awk -F"'" ' { print $2 } ' | xargs echo " "
+    curl -s  $url | fgrep "current-price" | awk -F">" ' { print substr($2,1,4) } ' | xargs echo " "
 }
-holePreise 'Brinkschulte_Monheim' http://www.clever-tanken.de/tankstelle_details/43461
-holePreise 'SB_KrischerStrasse55' http://www.clever-tanken.de/tankstelle_details/32862
-holePreise 'Shell_Opladener_str '  http://www.clever-tanken.de/tankstelle_details/5282
-holePreise 'SB_KoelnerLand_350  '  http://www.clever-tanken.de/tankstelle_details/52720
-holePreise 'PM_KoelnerLand_255  '  http://www.clever-tanken.de/tankstelle_details/15269
-holePreise 'Shell_KoelnerLand_77'  http://www.clever-tanken.de/tankstelle_details/45807
-holePreise 'Esso_Ross_Str_86_DUS'  http://www.clever-tanken.de/tankstelle_details/8913
+holePreise 'Brinkschulte_Monheim' https://www.clever-tanken.de/tankstelle_details/43461
+#holePreise 'SB_KrischerStrasse55' https://www.clever-tanken.de/tankstelle_details/32862
+holePreise 'Shell_Opladener_str '  https://www.clever-tanken.de/tankstelle_details/5282
+holePreise 'SB_KoelnerLand_350  '  https://www.clever-tanken.de/tankstelle_details/52720
+holePreise 'PM_KoelnerLand_255  '  https://www.clever-tanken.de/tankstelle_details/145240 15269
+holePreise 'Shell_KoelnerLand_77'  https://www.clever-tanken.de/tankstelle_details/45807
+holePreise 'Esso_Ross_Str_86_DUS'  https://www.clever-tanken.de/tankstelle_details/8913
